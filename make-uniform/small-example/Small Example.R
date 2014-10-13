1ebc19c8c346c0d0db2e30e74d6434d9a47cd407
DavidOry <dory@mtc.ca.gov>
2014-10-13 16:07:59 -0700

    cleaned up small example; early days on production

--- make-uniform/small-example/Small Example.R
+++ make-uniform/small-example/Small Example.R
@@ -4,30 +4,19 @@
 # with common variables and common responses.  This small example builds off a mini example (../mini-example) and 
 # starts to work with real data.
 
-# TODO add variables of different types as we go
-
 # Overhead
 library(reshape2)
 suppressMessages(library(dplyr))
 library(stringr)
 
+# Parameters
+survey.A.key <- 'ETC'
+survey.B.key <- 'RED'
+
 # Data reads -- two survey data sets and one dictionary file
 survey.A <- read.csv('ETC Example No Locations.csv', header = TRUE)
 survey.B <- read.csv('RED Example No Locations.csv', header= TRUE)
 dictionary.all <- read.csv('Example Dictionary.csv', header = TRUE)
-
-# Reshape survey A into a four column database - ID, Variable, Response, Survey
-survey.A.melt <- melt(survey.A, id = 'ID')
-survey.A.melt <- select(survey.A.melt, ID, Survey_Variable = variable, Survey_Response = value)
-survey.A.melt <- mutate(survey.A.melt, Survey = 'ETC')
-
-# Reshape survey B into a four column database - ID, Variable, Response, Survey
-survey.B.melt <- melt(survey.B, id = 'ID')
-survey.B.melt <- select(survey.B.melt, ID, Survey_Variable = variable, Survey_Response = value)
-survey.B.melt <- mutate(survey.B.melt, Survey = 'RED')
-
-# Bind survey data A and B
-survey.combine <- rbind(survey.A.melt, survey.B.melt)
 
 # Prepare seperate dictionaries for categorical and non-categorical variables
 dictionary.non <- dictionary.all %.%
@@ -37,6 +26,35 @@ dictionary.non <- dictionary.all %.%
 dictionary.cat <- dictionary.all %.%
   filter(Generic_Response != 'NONCATEGORICAL') %.%
   mutate(Survey_Response = as.character(Survey_Response))
+
+# Select the data in the dictionary for each survey
+dictionary.A <- dictionary.all %.%
+  filter(Survey == survey.A.key)
+
+variables.A <- as.character(factor(unique(unlist(dictionary.A$Survey_Variable, use.names = TRUE))))
+
+survey.A.select <- survey.A[ ,variables.A]
+
+
+dictionary.B <- dictionary.all %.%
+  filter(Survey == survey.B.key)
+
+variables.B <- as.character(factor(unique(unlist(dictionary.B$Survey_Variable, use.names = TRUE))))
+
+survey.B.select <- survey.B[ ,variables.B]
+
+# Reshape survey A into a four column database - ID, Variable, Response (as a string), Survey
+survey.A.melt <- melt(survey.A.select, id = 'ID')
+survey.A.melt <- select(survey.A.melt, ID, Survey_Variable = variable, Survey_Response = value)
+survey.A.melt <- mutate(survey.A.melt, Survey = survey.A.key)
+
+# Reshape survey B into a four column database - ID, Variable, Response, Survey
+survey.B.melt <- melt(survey.B.select, id = 'ID')
+survey.B.melt <- select(survey.B.melt, ID, Survey_Variable = variable, Survey_Response = value)
+survey.B.melt <- mutate(survey.B.melt, Survey = survey.B.key)
+
+# Bind survey data A and B
+survey.combine <- rbind(survey.A.melt, survey.B.melt)
 
 # Join the dictionary and prepare the categorical variables
 survey.cat <- mutate(survey.combine, Survey_Response = as.character(Survey_Response))
