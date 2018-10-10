ef8e7c070dfe2d012f19f3a0a955046c38d69d07
John Helsel <john.helsel@wsp.com>
2018-10-10 13:50:07 -0700

    Refactor decomposition script

--- decomposition/DecompositionAnalysis_SI.r
+++ decomposition/DecompositionAnalysis_SI.r
@@ -1,100 +1,151 @@
 ##################################################################################################
 ### Script to implement decomposition analysis on MTC survey data
 ### Author: Shimon Israel, February 2018, based on Binny M Paul, binny.paul@rsginc.com, April 2016
+### Updated: John Helsel, john.helsel@wsp.com, October 2018
 ##################################################################################################
 
-suppressMessages(library(dplyr))
+# Libraries and Options
+list_of_packages <- c("tidyverse")
+
+new_packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"Package"])]
+
+if(length(new_packages)) install.packages(new_packages)
+
+for (p in list_of_packages){
+  library(p, character.only = TRUE)
+}
+
+options(stringsAsFactors = FALSE)
 
 # User Inputs
-OBS_Dir <- "M:/Data/OnBoard/Data and Reports/_data Standardized/decomposition"
-Legacy <- "M:/Data/OnBoard/Data and Reports/_data Standardized/survey_legacy.RData"
-load(Legacy)
+# New users should add their own path to the relevant data in user_list
+user_list <- data.frame(
+  user = c("helseljw"), 
+  path = c("../Data and Reports/")
+)
 
-setwd(OBS_Dir)
+me <- Sys.getenv("USERNAME")
+dir_path <- user_list %>%
+  filter(user == me) %>%
+  .$path
+
+# File Paths
+obs_data_path <- paste0(dir_path, "_data Standardized/decomposition/survey_decomposition.RData")
+legacy_data_path <- paste0(dir_path, "_data Standardized/survey_legacy.RData")
+decom_output_path <- paste0(dir_path, "_data Standardized/decomposition/DecompositionAnalysis.csv")
 
 # Read Files
-load("survey_decomposition.RData")
+load(legacy_data_path)
+load(obs_data_path)
 
 # Rename operators so everything matches and filter weekday records only
 
-OBS <- survey.decomposition %>% mutate(
-  operator=ifelse(operator=="SF Muni","MUNI",operator),
-  operator=ifelse(operator=="Caltrain","CALTRAIN",operator),
-  operator=ifelse(operator=="Napa Vine","NAPA VINE",operator),
-  operator=ifelse(operator=="Napa Vine","NAPA VINE",operator)) %>%
-  filter(weekpart=="WEEKDAY") # Weekday records only 
+obs <- survey_decomposition %>% 
+  mutate(operator = toupper(operator),
+         operator = ifelse(operator == "SF MUNI", "MUNI", operator)
+         ) %>%
+  filter(weekpart == "WEEKDAY") # Weekday records only 
 
-#Fix missing values
-#OBS$TRANSFERS_FROM_CODE[is.na(OBS$TRANSFERS_FROM_CODE)] <- 0 #missing for dummy records
-#OBS$TRANSFERS_TO_CODE[is.na(OBS$TRANSFERS_TO_CODE)] <- 0 #missing for dummy records
 
 # Decomposition Analysis
-DA_Table <- data.frame(unique(OBS$operator))
-colnames(DA_Table) <- c("operator")
-DA_Table$SURVEYED_RESP <- 0
-DA_Table$TRNSF_FROM_RESP <- 0
-DA_Table$TRNSF_TO_RESP <- 0
+da_table <- data.frame("operator" = unique(obs$operator))
+da_table <- da_table %>%
+  mutate(surveyed_resp = 0,
+         trnsf_from_resp = 0,
+         trnsf_to_resp = 0,
+         t1 = 0,
+         t2 = 0,
+         t3 = 0,
+         t4 = 0,
+         f1 = 0,
+         f2 = 0,
+         f3 = 0,
+         f4 = 0)
 
-DA_Table$T1 <- 0
-DA_Table$T2 <- 0
-DA_Table$T3 <- 0
-DA_Table$T4 <- 0
+temp <- obs %>%
+  filter(first_before_operator %in% da_table$operator) %>%
+  group_by(operator) %>%
+  summarise(trip_weight = sum(trip_weight))
+da_table <- da_table %>%
+  left_join(temp, by = "operator") %>%
+  mutate(surveyed_resp = trip_weight) %>% 
+  select(-trip_weight)
 
-DA_Table$F1 <- 0
-DA_Table$F2 <- 0
-DA_Table$F3 <- 0
-DA_Table$F4 <- 0
+temp <- obs %>%
+  filter(first_before_operator %in% da_table$operator) %>%
+  group_by(first_before_operator) %>%
+  summarise(trip_weight = sum(trip_weight))
+da_table <- da_table %>%
+  left_join(temp, by = c("operator" = "first_before_operator")) %>%
+  mutate(f1 = trip_weight) %>% 
+  select(-trip_weight)
 
+temp <- obs %>%
+  filter(second_before_operator %in% da_table$operator) %>%
+  group_by(second_before_operator) %>%
+  summarise(trip_weight = sum(trip_weight))
+da_table <- da_table %>%
+  left_join(temp, by = c("operator" = "second_before_operator")) %>%
+  mutate(f2 = trip_weight) %>% 
+  select(-trip_weight)
 
-temp <- aggregate(trip_weight~operator, data = OBS, FUN = sum)
-DA_Table$SURVEYED_RESP <- temp$trip_weight[match(DA_Table$operator, temp$operator)]
+temp <- obs %>%
+  filter(third_before_operator %in% da_table$operator) %>%
+  group_by(third_before_operator) %>%
+  summarise(trip_weight = sum(trip_weight))
+da_table <- da_table %>%
+  left_join(temp, by = c("operator" = "third_before_operator")) %>%
+  mutate(f3 = trip_weight) %>% 
+  select(-trip_weight)
 
-temp <- aggregate(trip_weight~first_before_operator, data = OBS[OBS$first_before_operator %in% DA_Table$operator,], FUN = sum)
-DA_Table$F1 <- temp$trip_weight[match(DA_Table$operator, temp$first_before_operator)]
+temp <- obs %>%
+  filter(first_after_operator %in% da_table$operator) %>%
+  group_by(first_after_operator) %>%
+  summarise(trip_weight = sum(trip_weight))
+da_table <- da_table %>%
+  left_join(temp, by = c("operator" = "first_after_operator")) %>%
+  mutate(t1 = trip_weight) %>% 
+  select(-trip_weight)
 
-temp <- aggregate(trip_weight~second_before_operator, data = OBS[OBS$second_before_operator %in% DA_Table$operator,], FUN = sum)
-DA_Table$F2 <- temp$trip_weight[match(DA_Table$operator, temp$second_before_operator)]
+temp <- obs %>%
+  filter(second_after_operator %in% da_table$operator) %>%
+  group_by(second_after_operator) %>%
+  summarise(trip_weight = sum(trip_weight))
+da_table <- da_table %>%
+  left_join(temp, by = c("operator" = "second_after_operator")) %>%
+  mutate(t2 = trip_weight) %>% 
+  select(-trip_weight)
 
-temp <- aggregate(trip_weight~third_before_operator, data = OBS[OBS$third_before_operator %in% DA_Table$operator,], FUN = sum)
-DA_Table$F3 <- temp$trip_weight[match(DA_Table$operator, temp$third_before_operator)]
+temp <- obs %>%
+  filter(third_after_operator %in% da_table$operator) %>%
+  group_by(third_after_operator) %>%
+  summarise(trip_weight = sum(trip_weight))
+da_table <- da_table %>%
+  left_join(temp, by = c("operator" = "third_after_operator")) %>%
+  mutate(t3 = trip_weight) %>% 
+  select(-trip_weight)
 
-#temp <- aggregate(trip_weight~fourth_before_operator, data = OBS[OBS$fourth_before_operator %in% DA_Table$operator,], FUN = sum)
-#DA_Table$F4 <- temp$trip_weight[match(DA_Table$operator, temp$fourth_before_operator)]
+da_table[is.na(da_table)] <- 0
 
-temp <- aggregate(trip_weight~first_after_operator, data = OBS[OBS$first_after_operator %in% DA_Table$operator,], FUN = sum)
-DA_Table$T1 <- temp$trip_weight[match(DA_Table$operator, temp$first_after_operator)]
+da_table <- da_table %>%
+  mutate(trnsf_from_resp = f1 + f2 + f3 + f4,
+         trnsf_to_resp = t1 + t2 + t3 + t4
+         )
 
-temp <- aggregate(trip_weight~second_after_operator, data = OBS[OBS$second_after_operator %in% DA_Table$operator,], FUN = sum)
-DA_Table$T2 <- temp$trip_weight[match(DA_Table$operator, temp$second_after_operator)]
-
-temp <- aggregate(trip_weight~third_after_operator, data = OBS[OBS$third_after_operator %in% DA_Table$operator,], FUN = sum)
-DA_Table$T3 <- temp$trip_weight[match(DA_Table$operator, temp$third_after_operator)]
-
-#temp <- aggregate(trip_weight~fourth_after_operator, data = OBS[OBS$fourth_after_operator %in% DA_Table$operator,], FUN = sum)
-#DA_Table$T4 <- temp$trip_weight[match(DA_Table$operator, temp$fourth_after_operator)]
-
-DA_Table[is.na(DA_Table)] <- 0
-
-DA_Table$TRNSF_FROM_RESP <- DA_Table$F1 + DA_Table$F2 + DA_Table$F3 + DA_Table$F4
-
-DA_Table$TRNSF_TO_RESP <- DA_Table$T1 + DA_Table$T2 + DA_Table$T3 + DA_Table$T4
-
-DA_Table <- DA_Table[,c("operator", "SURVEYED_RESP", "TRNSF_FROM_RESP", "TRNSF_TO_RESP")]
-
-DA_Table$operator <- as.character(DA_Table$operator)
-
+da_table <- da_table %>%
+  select(operator, surveyed_resp, trnsf_from_resp, trnsf_to_resp)
 
 # Process unlinked boardings
 
-Boardings <- aggregate(weight~operator, data = OBS, FUN = sum)
+boardings <- obs %>%
+  group_by(operator) %>%
+  summarise(weight = sum(weight))
 
-DA_Table$OBSERVED <- Boardings$weight[match(DA_Table$operator, Boardings$operator)]
+da_table <- da_table %>%
+  left_join(boardings, by = "operator") %>%
+  rename(observed = weight)
 
-DA_Table <- DA_Table[order(DA_Table$operator),]
-write.csv(DA_Table, paste(OBS_Dir, "DecompositionAnalysis.csv", sep = "/"), row.names = FALSE)
+da_table <- da_table %>%
+  arrange(operator)
 
-
-#View(OBS[,c("ID","FROM1", "FROM2", "FROM3", "FROM4", "operator", 
-#            "TO1", "TO2", "TO3", "TO4", "TRANSFERS_FROM_CODE", "TRANSFERS_TO_CODE", 
-#            "UNLINKED_WEIGHT_FACTOR", "trip_weight")])
-
+write.csv(da_table, decom_output_path, row.names = FALSE)
