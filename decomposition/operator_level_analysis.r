4cc7ccce71395b8dca19fbe6376f7b71910872c0
David Ory <david.ory@gmail.com>
2018-10-17 09:20:39 -0400

    cleaned up, checked against existing

--- decomposition/operator_level_analysis.r
+++ decomposition/operator_level_analysis.r
@@ -1,3 +1,5 @@
+# operator-level decomposition analysis
+
 # Libraries and Options --------------------------------------------------------
 list_of_packages <- c("tidyverse")
 
@@ -11,8 +13,7 @@ for (p in list_of_packages){
 
 options(stringsAsFactors = FALSE)
 
-# User Inputs ------------------------------------------------------------------
-# New users should add their own path to the relevant data in user_list
+# User-specific Dir ------------------------------------------------------------
 user_list <- data.frame(
   user = c("helseljw", 
            "USDO225024"), 
@@ -37,40 +38,16 @@ survey_decomposition <- readRDS(obs_data_path)
 obs_df <- survey_decomposition %>% 
   mutate(operator = toupper(operator),
          operator = ifelse(operator == "SF MUNI", "MUNI", operator)) %>%
-  filter(weekpart == "WEEKDAY") # Weekday records only 
+  filter(weekpart == "WEEKDAY")
 
 
 # Decomposition Analysis -------------------------------------------------------
-# TODO revisit -- why 0 and not NA?
-decom_df <- data.frame(operator = unique(obs_df$operator)) %>%
-  mutate(surveyed_resp = 0,
-         trnsf_from_resp = 0,
-         trnsf_to_resp = 0,
-         t1 = 0,
-         t2 = 0,
-         t3 = 0,
-         t4 = 0,
-         f1 = 0,
-         f2 = 0,
-         f3 = 0,
-         f4 = 0)
-  
-join_df <- obs_df %>%
-  filter(first_before_operator %in% decom_df$operator) %>%
+working_df <- obs_df %>%
   group_by(operator) %>%
-  summarise(trip_weight = sum(trip_weight))
+  summarise(surveyed_resp = sum(trip_weight))
 
-decom_df <- decom_df %>%
-  left_join(join_df, by = "operator") %>%
-  mutate(surveyed_resp = trip_weight) %>% 
-  select(-trip_weight)
 
 add_sequence <- function(observed_df, running_df, sequence_name_string, weight_name_string) {
-  
-  # observed_df <- obs_df
-  # running_df <- decom_df
-  # sequence_name_string <- 'first_before_operator'
-  # weight_name_string <- 'f1'
   
   vars <- c(operator = sequence_name_string)
   
@@ -83,6 +60,7 @@ add_sequence <- function(observed_df, running_df, sequence_name_string, weight_n
   
   return_df <- running_df %>%
     left_join(join_df, by = c("operator")) %>%
+    mutate(trip_weight = ifelse(is.na(trip_weight), 0, trip_weight)) %>%
     mutate(!!weight_name_string := trip_weight) %>%
     select(-trip_weight)
 
@@ -91,37 +69,30 @@ add_sequence <- function(observed_df, running_df, sequence_name_string, weight_n
   
 }
 
-decom_df <- add_sequence(obs_df, decom_df, 'first_before_operator',  'f1')
-decom_df <- add_sequence(obs_df, decom_df, 'second_before_operator', 'f2')
-decom_df <- add_sequence(obs_df, decom_df, 'third_before_operator',  'f3')
+working_df <- add_sequence(obs_df, working_df, 'first_before_operator',  'before_01')
+working_df <- add_sequence(obs_df, working_df, 'second_before_operator', 'before_02')
+working_df <- add_sequence(obs_df, working_df, 'third_before_operator',  'before_03')
 
-decom_df <- add_sequence(obs_df, decom_df, 'first_after_operator',   't1')
-decom_df <- add_sequence(obs_df, decom_df, 'second_after_operator',  't2')
-decom_df <- add_sequence(obs_df, decom_df, 'second_after_operator',  't3')
+working_df <- add_sequence(obs_df, working_df, 'first_after_operator',   'after_01')
+working_df <- add_sequence(obs_df, working_df, 'second_after_operator',  'after_02')
+working_df <- add_sequence(obs_df, working_df, 'third_after_operator',  'after_03')
 
-# START HERE 
-
-decom_df[is.na(da_table)] <- 0
-
-da_table <- da_table %>%
-  mutate(trnsf_from_resp = f1 + f2 + f3 + f4,
-         trnsf_to_resp = t1 + t2 + t3 + t4
-         )
-
-da_table <- da_table %>%
+working_df <- working_df %>%
+  mutate(trnsf_from_resp = before_01 + before_02 + before_03) %>%
+  mutate(trnsf_to_resp = after_01 + after_02 + after_03) %>%
   select(operator, surveyed_resp, trnsf_from_resp, trnsf_to_resp)
 
-# Process unlinked boardings
 
-boardings <- obs %>%
+# Process unlinked boardings --------------------------------------------------
+boardings_df <- obs_df %>%
   group_by(operator) %>%
   summarise(weight = sum(weight))
 
-da_table <- da_table %>%
-  left_join(boardings, by = "operator") %>%
-  rename(observed = weight)
-
-da_table <- da_table %>%
+working_df <- working_df %>%
+  left_join(boardings_df, by = "operator") %>%
+  rename(observed = weight) %>%
   arrange(operator)
 
-write.csv(da_table, decom_output_path, row.names = FALSE)
+write.csv(working_df, output_path, row.names = FALSE)
+
+
