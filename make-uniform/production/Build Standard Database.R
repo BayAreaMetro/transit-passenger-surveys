8990aa6a6708e2c7232a576d7f3d4d2964069251
Yuqi Wang <ywang@bayareametro.gov>
2021-04-09 16:14:52 -0700

    revert output file format change back to RDS

--- make-uniform/production/Build Standard Database.R
+++ make-uniform/production/Build Standard Database.R
@@ -131,15 +131,15 @@ f_capitolcorridor2019_survey_path <- paste0(dir_path,
 
 today = Sys.Date()
 f_output_rds_path <- paste0(dir_path,
-                            "_data Standardized/survey_standard_", today, ".RData")
+                            "_data Standardized/survey_standard_", today, ".RDS")
 f_output_csv_path <- paste0(dir_path,
                             "_data Standardized/survey_standard_", today, ".csv")
 f_ancillary_output_rdata_path <- paste0(dir_path,
-                                        "_data Standardized/ancillary_variable_", today, ".RData")
+                                        "_data Standardized/ancillary_variable_", today, ".RDS")
 f_ancillary_output_csv_path <- paste0(dir_path,
                                       "_data Standardized/ancillary_variables_", today, ".csv")
 f_output_decom_rdata_path <- paste0(dir_path,
-                                    "_data Standardized/decomposition/survey_decomposition_", today, ".RData")
+                                    "_data Standardized/decomposition/survey_decomposition_", today, ".RDS")
 f_output_decom_csv_path <- paste0(dir_path,
                                   "_data Standardized/decomposition/survey_decomposition_", today, ".csv")
 
