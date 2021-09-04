04e30e89f93ede338ed9a9d66409e5e5a56a3093
Shimon Israel <sisrael@bayareametro.gov>
2021-09-03 17:02:29 -0700

    Replaced tabs with spaces for table formatting

--- requests/Code Transit Passenger Discount by Person Type.R
+++ requests/Code Transit Passenger Discount by Person Type.R
@@ -143,15 +143,15 @@ missing_discount <- read_excel(file.path(wd,"Operator Discounts for 2015.xlsx"),
 # Person type categories, 1 and 2 need to be collapsed based on available data
 
 '
-NUMBER  PERSON-TYPE	                    AGE 	  WORK STATUS	  SCHOOL STATUS
-1	      Full-time worker (30 hours+)	  18+ 	  Full-time	    None
-2	      Part-time worker (<30 hours)	  18+ 	  Part-time	    None
-3	      College student	                18+	    Any	          College +
-4	      Non-working adult	              18 - 64	Unemployed	  None
-5	      Non-working senior	            65+	    Unemployed	  None
-6	      Driving age student 	          16-17	  Any	          Pre-college
-7	      Non-driving student	            6 - 15	None	        Pre-college
-8	      Pre-school	                    0-5	    None	        None
+NUMBER  PERSON-TYPE                     AGE     WORK STATUS   SCHOOL STATUS
+1	      Full-time worker (30 hours+)    18+     Full-time     None
+2	      Part-time worker (<30 hours)    18+     Part-time     None
+3	      College student                 18+     Any           College +
+4	      Non-working adult               18 - 64 Unemployed    None
+5	      Non-working senior              65+     Unemployed    None
+6	      Driving age student             16-17   Any           Pre-college
+7	      Non-driving student             6 - 15  None          Pre-college
+8	      Pre-school                      0-5     None          None
 '
 
 final <- rbind(discount_summary,missing_discount) %>% 
