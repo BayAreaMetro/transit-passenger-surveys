0d0822bbe97913bce8573ca78dea97d561be8910
DavidOry <david.ory@gmail.com>
2023-02-07 12:46:47 -0500

    replace `Rmd` with `R`

--- summaries/make-acceptance-criteria-summaries.R
+++ summaries/make-acceptance-criteria-summaries.R
@@ -1,10 +1,8 @@
----
-title: "Make Acceptance Criteria Summaries"
-output: html_notebook
----
-
-# Overhead
-```{r overhead, include = FALSE}
+#-------------------------------------------------------------------------------
+#title: "Make Acceptance Criteria Summaries"
+#-------------------------------------------------------------------------------
+  
+# Overhead ---------------------------------------------------------------------
 packages_vector <- c("tidyverse")
 
 need_to_install <- packages_vector[!(packages_vector %in% installed.packages()[,"Package"])]
@@ -15,38 +13,31 @@ for (package in packages_vector) {
   library(package, character.only = TRUE)
 }
 
-```
-
-# Remote I-O
-```{r remote-io}
+# Remote I-O -------------------------------------------------------------------
 box_dir <- "~/Box Sync/"
-
 survey_filename <- paste0(box_dir, "Survey_Database_122717/survey.Rdata")
+output_filename <- paste0(
+  box_dir, 
+  "Survey_Database_122717/acceptance-criteria-summaries-year-2015.csv"
+)
 
-output_filename <- paste0(box_dir, "Survey_Database_122717/acceptance-criteria-summaries-year-2015.csv")
-```
-
-# Parameters
-```{r parameters}
-time_period_dict_df <- tibble(day_part = c("EARLY AM", "AM PEAK", "MIDDAY", "PM PEAK", "EVENING", "NIGHT"),
-                              time_period = c("ea", "am", "md", "pm", "ev", "ev"))
-
+# Parameters -------------------------------------------------------------------
+time_period_dict_df <- tibble(
+  day_part = c("EARLY AM", "AM PEAK", "MIDDAY", "PM PEAK", "EVENING", "NIGHT"),
+  time_period = c("ea", "am", "md", "pm", "ev", "ev")
+)
 rail_operators_vector <- c("BART","Caltrain","ACE","Sonoma-Marin Area Rail Transit")
-
 ALL_DAY_WORD <- "daily"
-
 survey_years_to_summarise <- seq(from = 2012, to = 2017)
-```
 
-# Methods
-```{r methods}
+# Methods ----------------------------------------------------------------------
 make_direction_from_route <- function(input_df, input_reg_ex_word, brackets_bool) {
   
   if (brackets_bool) {
     replace_word <- trimws(gsub("\\[|\\]", "", input_reg_ex_word))
   }
   else {
-      replace_word <- input_reg_ex_word
+    replace_word <- input_reg_ex_word
   }
   
   return_df <- input_df %>%
@@ -55,20 +46,13 @@ make_direction_from_route <- function(input_df, input_reg_ex_word, brackets_bool
     mutate(direction = if_else(flag, replace_word, direction)) 
   
   return(return_df)
-    
+  
 }
 
-```
-
-
-# Data Reads
-```{r data-reads}
+# Data Reads -------------------------------------------------------------------
 load(survey_filename)
-```
 
-
-# Reductions
-```{r reductions}
+# Reductions -------------------------------------------------------------------
 by_time_period_df <- survey %>%
   filter(weekpart != "WEEKEND") %>%
   filter(survey_year %in% survey_years_to_summarise) %>%
@@ -91,16 +75,13 @@ daily_df <- by_time_period_df %>%
   summarise(survey_boardings = sum(survey_boardings), .groups = "drop") %>%
   mutate(direction = NA) %>%
   mutate(time_period = ALL_DAY_WORD)
-  
+
 output_df <- bind_rows(by_time_period_df, daily_df) %>%
   rename(survey_operator = operator,
          survey_route = route,
          survey_direction = direction)
-  
-```
 
-# Write
-```{r write}
+# Write ------------------------------------------------------------------------
 write_csv(output_df, output_filename)
-```
+
 
