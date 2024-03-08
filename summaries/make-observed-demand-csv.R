6cc7570c55e2a85ceb1290d8bdd8846ed14c513f
DavidOry <david.ory@gmail.com>
2024-03-08 13:48:37 -0500

    update observed demand summaries with additional summary variations

--- summaries/make-observed-demand-csv.R
+++ summaries/make-observed-demand-csv.R
@@ -14,12 +14,17 @@ for (package in packages_vector) {
 }
 
 # Remote I-O -------------------------------------------------------------------
-box_dir <- "~/Box Sync/"
+box_dir <- "~/Box Sync/" 
+github_dir <- "../../"
 survey_filename <- paste0(box_dir, "Survey_Database_090221/TPS_Model_Version_PopulationSim_Weights2021-09-02.Rdata")
 output_filename <- paste0(
   box_dir, 
   "Survey_Database_090221/observed-demand-year-2015.csv"
 )
+output_01_filename <- paste0(box_dir, "Survey_Database_090221/observed-demand-year-2015-emme-taz-by-path.csv")
+
+# TODO: need to update to more stable path
+tm2_zone_system_filename <- paste0(github_dir, "tm2py/examples/temp_acceptance/inputs/landuse/taz_data.csv") 
 
 
 # Parameters -------------------------------------------------------------------
@@ -31,7 +36,17 @@ time_period_dict_df <- tibble(
 # Methods ----------------------------------------------------------------------
 
 # Data Reads -------------------------------------------------------------------
-load(survey_filename)
+load(survey_filename) 
+
+taz_df <- read_csv(tm2_zone_system_filename, col_types = cols(
+  TAZ = col_integer(),
+  index = col_double(),
+  TAZ_ORIGINAL = col_integer(),
+  AVGTTS = col_double(),
+  DIST = col_double(),
+  PCTDETOUR = col_double(),
+  TERMINALTIME = col_double()
+))
 
 # Reductions -------------------------------------------------------------------
 output_df <- TPS %>%
@@ -47,3 +62,38 @@ sum(output_df$trips)
 # Write ------------------------------------------------------------------------
 write_csv(output_df, output_filename)
 
+# Variations -------------------------------------------------------------------
+taz_dict_df <- select(taz_df, external_taz = TAZ_ORIGINAL, emme_taz = TAZ)
+
+## Variation 1: Emme TAZ numbers, aggregate over TAZs by path and time-of-day --
+working_df <- output_df %>%
+  left_join(., 
+            select(taz_dict_df, orig_tm2_taz = external_taz, emme_taz), 
+            by = c("orig_tm2_taz")) %>%
+  rename(orig_emme_taz = emme_taz) %>%
+  left_join(., 
+            select(taz_dict_df, dest_tm2_taz = external_taz, emme_taz), 
+            by = c("dest_tm2_taz")) %>%
+  rename(dest_emme_taz = emme_taz) %>%
+  select(model_time, 
+         access_mode_model, 
+         egress_mode_model, 
+         orig_emme_taz, 
+         dest_emme_taz, 
+         trips) %>%
+  mutate(path_type = if_else(access_mode_model == "walk" & egress_mode_model == "walk", "wlk_trn_wlk", "drive-to-drive")) %>%
+  mutate(path_type = if_else(access_mode_model == "pnr" & egress_mode_model == "walk", "pnr_trn_wlk", path_type)) %>%
+  mutate(path_type = if_else(access_mode_model == "knr" & egress_mode_model == "walk", "knr_trn_wlk", path_type)) %>%
+  mutate(path_type = if_else(access_mode_model == "tnc" & egress_mode_model == "walk", "knr_trn_wlk", path_type)) %>%
+  mutate(path_type = if_else(access_mode_model == "walk" & egress_mode_model == "pnr", "wlk_trn_pnr", path_type)) %>%
+  mutate(path_type = if_else(access_mode_model == "walk" & egress_mode_model == "knr", "wlk_trn_knr", path_type)) %>%
+  mutate(path_type = if_else(access_mode_model == "walk" & egress_mode_model == "tnc", "wlk_trn_knr", path_type)) %>%
+  filter(path_type != "drive-to-drive")
+
+output_01_df <- working_df %>%
+  group_by(model_time, path_type, orig_emme_taz, dest_emme_taz) %>%
+  summarise(trips = sum(trips), .groups = "drop")
+
+sum(output_01_df$trips)
+
+write_csv(output_01_df, output_01_filename)
