a3d745b350c7c4434d7f4cdad3817b2312253a7d
Lisa Zorn <lzorn@bayareametro.gov>
2024-09-11 17:56:18 -0700

    Fix bug that was causing the rbind to create a gigantic output dataset
    Instead of a single NA in the legacy dataset for each variable, a huge string of NAs was being created.  I'm not sure why it started happening but I fixed by switching to dplyr::bind_rows() which
    doesn't require the columns to match exactly. But it did require some explicit type matching, which I added by coercing the legacy types to the standard types.

--- make-uniform/production/Combine_Legacy_Standard_Surveys.R
+++ make-uniform/production/Combine_Legacy_Standard_Surveys.R
@@ -39,27 +39,39 @@ combine_data <- function(data_standard,
   print('variables in standard_database but not in legacy_database:')
   for (std_name in std_names) {
     if(!(std_name %in% legacy_names)) {
-      print(std_name)
-      # New column in standardized data. Add NA column in legacy so rbind will succeed.
-      data_legacy[,std_name] <- NA
-      if (typeof(data_standard[,std_name])=="character") {
-        data_legacy[,std_name] <- as.character(data_legacy[,std_name])
-      } else if (typeof(data_standard[,std_name])=="double") {
-        data_legacy[,std_name] <- as.numeric(data_legacy[,std_name])
-      }
+      print(paste(" ",std_name))
     }
   }
   
   print('variables in legacy_database but not in standard_database:')
   for (legacy_name in legacy_names) {
     if(!(legacy_name %in% std_names)) {
-      print(legacy_name)
-      # Columns in legacy data but not in standardized data. Add NA column in legacy so rbind will succeed.
-      data_standard[,legacy_name] <- NA
-      if (typeof(data_legacy[,legacy_name])=="character") {
-        data_standard[,legacy_name] <- as.character(data_standard[,legacy_name])
-      } else if (typeof(data_legacy[,legacy_name])=="double") {
-        data_standard[,legacy_name] <- as.numeric(data_standard[,legacy_name])
+      print(paste(" ",legacy_name))
+    }
+  }
+
+  # check types
+  std_class <- sapply(data_standard,class)
+  legacy_class <- sapply(data_legacy,class)
+  for (col_name in std_names) {
+    if(col_name %in% legacy_names) {
+      if (std_class[col_name] != legacy_class[col_name]) {
+        print(paste("Mismatched classes for",col_name,
+        "; standard:",std_class[col_name],
+        "; legacy:",legacy_class[col_name]))
+        # convert to standard
+        if (std_class[col_name] == "numeric") {
+          # don't warn on NAs introduced by coercion
+          suppressWarnings(
+            data_legacy <- data_legacy %>% mutate(!!col_name := as.numeric(!!col_name))
+          )
+        }
+        else if (std_class[col_name] == "character") {
+          # don't warn on NAs introduced by coercion
+          suppressWarnings(
+            data_legacy <- data_legacy %>% mutate(!!col_name := as.character(!!col_name))
+          )
+        }
       }
     }
   }
@@ -68,7 +80,8 @@ combine_data <- function(data_standard,
   data_legacy['survey_batch'] = 'legacy'
   data_standard['survey_batch'] = 'standard'
   
-  data_combine <- rbind(data_standard, data_legacy)
+  # this fills missing columns with NAs
+  data_combine <- dplyr::bind_rows(data_standard, data_legacy)
   
   # Remove the BART pre-test data
   data_combine <- data_combine %>%
