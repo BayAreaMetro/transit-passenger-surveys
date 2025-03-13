dae399bffd1a11ea75f374a257a997741207be8c
Shimon Israel <SIsrael@bayareametro.gov>
2025-03-13 11:22:02 -0700

    Added totals in addition to operator-level summaries.

--- requests/TIP 2023 Income and Race_Ethnicity by Operator.r
+++ requests/TIP 2023 Income and Race_Ethnicity by Operator.r
@@ -12,7 +12,7 @@ options(scipen = 999)
 
 # Input TPS
 
-TPS_SURVEY_IN = "M:/Data/OnBoard/Data and Reports/_data Standardized/share_data/model_version/TPS_Model_Version_PopulationSim_Weights2021-09-02.Rdata"
+TPS_SURVEY_IN = "M:/Data/OnBoard/Data and Reports/_data_Standardized/share_data/model_version/TPS_Model_Version_PopulationSim_Weights2021-09-02.Rdata"
 load (TPS_SURVEY_IN)
 
 # Output location
@@ -113,8 +113,52 @@ survey_race <- TPS %>%
   summarize(total=sum(final_boardWeight_2015)) %>% 
   spread(race_rc,total)
 
+# Now do totals as opposed to operators
+
+survey_income_total <- TPS %>% 
+  filter(!(household_income %in% c("under $35,000","Missing","refused","$35,000 or higher"))) %>% 
+  filter(!(is.na(household_income))) %>% 
+  mutate(
+    income_rc=case_when(
+      household_income=="under $10,000"        ~"1_less than 25k",
+      household_income=="$10,000 to $25,000"   ~"1_less than 25k",
+      household_income=="under $25,000"        ~"1_less than 25k",
+      household_income=="$25,000 to $35,000"   ~"2_25-50k",
+      household_income=="$35,000 to $50,000"   ~"2_25-50k",
+      household_income=="$25,000 to $50,000"   ~"2_25-50k",
+      household_income=="$50,000 to $75,000"   ~"3_50-75k",
+      household_income=="$75,000 to $100,000"  ~"4_75-100k",
+      household_income=="$100,000 to $150,000" ~"5_100-150k",
+      household_income=="$150,000 or higher"   ~"6_150k+",
+      TRUE                           ~"Not coded"
+    )
+  ) %>% 
+  group_by(income_rc) %>% 
+  summarize(total=sum(final_boardWeight_2015)) %>% 
+  spread(income_rc,total)
+
+# Race
+
+survey_race_total <- TPS %>% 
+  filter(!(race=="Missing")) %>% 
+  mutate(
+    race_rc=case_when(
+      hispanic=="HISPANIC/LATINO OR OF SPANISH ORIGIN"  ~ "5_Hispanic",
+      race=="WHITE"                                     ~ "1_White",
+      race=="BLACK"                                     ~ "2_Black",
+      race=="ASIAN"                                     ~ "3_Asian",
+      race=="OTHER"                                     ~ "4_Other",
+      TRUE                                              ~ "Not coded"
+    )
+  ) %>% 
+  group_by(race_rc) %>% 
+  summarize(total=sum(final_boardWeight_2015)) %>% 
+  spread(race_rc,total)
+
 # Write out files
 
 write.csv(survey_income,file.path(Output,"survey_income.csv"),row.names = FALSE)
 write.csv(survey_race,file.path(Output,"survey_race.csv"),row.names = FALSE)
+write.csv(survey_income_total,file.path(Output,"survey_income_total.csv"),row.names = FALSE)
+write.csv(survey_race_total,file.path(Output,"survey_race_total.csv"),row.names = FALSE)
  
