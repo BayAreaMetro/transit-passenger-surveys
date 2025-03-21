55df02f6f0982a9996a2caf413140e0c75c0582f
Shimon Israel <sisrael@bayareametro.gov>
2025-03-21 08:36:47 -0700

    Create Find_Nearest_BART_Station_to_Destination.R

---
+++ exploration/Find_Nearest_BART_Station_to_Destination.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(geosphere)
+
+# Load BART stations data (latitude, longitude)
+bart_stations <- read_csv("bart_stations.csv")  # Assumes columns: station_id, lat, lon
+
+# Load passenger trip destinations (latitude, longitude)
+passenger_trips <- read_csv("passenger_trips.csv")  # Assumes columns: passenger_id, dest_lat, dest_lon
+
+# Function to find the nearest station
+find_nearest_station <- function(lat, lon, stations) {
+  stations %>%
+    mutate(distance = distHaversine(c(lon, lat), cbind(lon, lat))) %>%
+    slice_min(distance) %>%
+    pull(station_id)
+}
+
+# Apply function to each passenger trip
+passenger_trips <- passenger_trips %>%
+  rowwise() %>%
+  mutate(nearest_station = find_nearest_station(dest_lat, dest_lon, bart_stations)) %>%
+  ungroup()
+
+# Save results
+write_csv(passenger_trips, "passenger_trips_with_nearest_station.csv")
