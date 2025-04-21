76ed5498501797537d1a42a2f0da5f2cbf49696c
Shimon Israel <sisrael@bayareametro.gov>
2025-04-21 16:09:08 -0700

    Adding Golden Gate and ACE surveys

--- snapshot-survey/Snapshot_Survey_Income_Race_Summaries_Operator.R
+++ snapshot-survey/Snapshot_Survey_Income_Race_Summaries_Operator.R
@@ -1,6 +1,12 @@
 # Snapshot_Survey_Income_Race_Summaries_Operator.R
 # Calculate income and race summaries by operator
 
+"Use the following datasets to determine income and race/ethnicity for transit by operator:
+1. Snapshot Survey 2023/2024
+2. Golden Gate Transit Survey 2023 (completed by the operator, from 'preprocessed' version that includes MTC expansion weights)
+3. ACE 2023 (completed by the operator, from 'preprocessed' version that includes MTC expansion weights)
+"
+
 # Set options to get rid of scientific notation
 
 options(scipen = 999)
@@ -10,7 +16,7 @@ options(scipen = 999)
 suppressMessages(library(tidyverse))
 library(readxl)
 
-# Set file directories for input and output
+# Set file directories for input and output, bring in Snapshot Survey data
 
 userprofile    <- gsub("\\\\","/", Sys.getenv("USERPROFILE"))
 box_dir1       <- file.path(userprofile, "Box", "Modeling and Surveys","Surveys","Transit Passenger Surveys")
@@ -20,7 +26,15 @@ output_dir     <- file.path(userprofile,"Box","Plan Bay Area 2050+","Performance
 
 snapshot <- read_excel(box_dir2, sheet = "data file")
 
-race_income <- snapshot %>%
+# Set locations for Golden Gate Transit and ACE "preprocessed" data files, bring in data
+
+goldengate_in <- "M:/Data/OnBoard/Data and Reports/Golden Gate Transit/2023/GoldenGate_Transit_Ferry_preprocessed.csv"
+ace_in        <- "M:/Data/OnBoard/Data and Reports/ACE/2023/ACE_Onboard_preprocessed.csv"
+
+goldengate    <- read.csv(goldengate_in)
+ace           <- read.csv(ace_in)
+
+race_income_snapshot <- snapshot %>%
   mutate(race=case_when(
     Q19_1=="B" & if_all(Q19_2:Q19_4, is.na)             ~ "Missing",
     if_any(Q19_1:Q19_4, ~.== 4)                         ~ "Hispanic",
@@ -53,7 +67,7 @@ race_income <- snapshot %>%
 
 # Summarize income and race by operator for weekdays
 
-income_summary <- race_income %>% 
+income_summary_snapshot <- race_income_snapshot %>% 
   filter(Daytype=="DAY") %>% 
   group_by(System,income) %>% 
   summarize(total=sum(Weight),.groups = "drop") %>% 
@@ -63,13 +77,61 @@ income_summary <- race_income %>%
          "$80,000 to $99,999", "$100,000 to $149,999", "$150,000 to $199,999", 
          "$200,000 and above",  "Missing", "Multiple responses")
 
-race_summary <- race_income %>% 
+race_summary_snapshot <- race_income_snapshot %>% 
   filter(Daytype=="DAY") %>% 
   group_by(System,race) %>% 
   summarize(total=sum(Weight),.groups = "drop") %>% 
   pivot_wider(.,names_from = race,values_from = total) %>% 
   select("System","White, not Hispanic","Black, not Hispanic","Asian, not Hispanic","Other, not Hispanic","Hispanic", 
          "Missing" )
+
+
+# Golden Gate Transit summaries for income and race
+
+income_summary_goldengate <- goldengate %>% 
+  filter(Strata %in% c("AM PEAK", "MIDDAY", "PM PEAK", "EVENING", "AM OFF")) %>% 
+  mutate(income=recode(household_income,
+                       "1"="Under $10,000",
+                       "2"="$10,000 to $24,999",
+                       "3"="$25,000 to $34,999",
+                       "4"="$35,000 to $49,999",
+                       "5"="$50,000 to $74,999",
+                       "6"="$75,000 to $99,999",
+                       "7"="$100,000 to $149,999",
+                       "8"="$150,000 to $199,999",
+                       "9"="$200,000 and above",
+                       "0"="Missing",
+                       "10"="Multiple responses"
+  )) %>% 
+  group_by(income) %>% 
+  summarize(total=sum(weight),.groups = "drop") %>% 
+  pivot_wider(.,names_from = income,values_from = total) %>% 
+  mutate("System"="Golden Gate Transit") %>% 
+  relocate(System,.before = everything()) %>% 
+  select(System,"Under $10,000","$10,000 to $24,999", "$25,000 to $34,999", "$35,000 to $49,999", 
+         "$50,000 to $74,999", "$75,000 to $99,999", "$100,000 to $149,999", "$150,000 to $199,999", 
+         "$200,000 and above",  "Missing", "Multiple responses")
+
+race_summary_goldengate <- goldengate %>% 
+  filter(Strata %in% c("AM PEAK", "MIDDAY", "PM PEAK", "EVENING", "AM OFF")) %>% 
+  mutate(race=case_when(
+    hispanic==1                            ~ "Hispanic",
+    hispanic==0 & race_dmy_wht==1 & race_dmy_asn
+  )
+  
+  Q19_1 %in% c("7", "9") & if_all(Q19_2:Q19_4, ~ . == 0)    ~ "Other, not Hispanic",
+  if_any(Q19_2:Q19_4, ~ . != 0)                             ~ "Other, not Hispanic",
+  )) %>% 
+  group_by(income) %>% 
+  summarize(total=sum(weight),.groups = "drop") %>% 
+  pivot_wider(.,names_from = income,values_from = total) %>% 
+  mutate("System"="Golden Gate Transit") %>% 
+  relocate(System,.before = everything()) %>% 
+  select(System,"Under $10,000","$10,000 to $24,999", "$25,000 to $34,999", "$35,000 to $49,999", 
+         "$50,000 to $74,999", "$75,000 to $99,999", "$100,000 to $149,999", "$150,000 to $199,999", 
+         "$200,000 and above",  "Missing", "Multiple responses")
+
+
 
 
 # Output files
